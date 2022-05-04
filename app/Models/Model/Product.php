7eae1e1181b380414a5885c98bbc3897f322c366
Khadiza <khadizaakhi756@gmail.com>
2022-05-04 20:10:11 +0600

    product update

--- app/Models/Model/Product.php
+++ app/Models/Model/Product.php
@@ -3,12 +3,15 @@
 namespace App\Models\Model;
 use App\Models\Model\Review;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    // use HasFactory;
+    use HasFactory;
+    protected $fillable=[
+        'name','detail','stock','price','discount'
+    ];
     public function reviews(){
         return $this->hasMany(Review::class);
     }
