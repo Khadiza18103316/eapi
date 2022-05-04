95c09c8c540534314ea29065b93bdf462d5ec976
Khadiza <khadizaakhi756@gmail.com>
2022-05-04 16:35:47 +0600

    product created

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
